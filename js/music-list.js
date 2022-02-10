a4b09f6ad398f7642c14e8913c8999daa4a51e07
anishsingh935 <anish.singh7310@gmail.com>
2022-02-10 14:09:01 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "music-1",
+        artist: "Jordan Scho",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "music-2",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "music-3",
+        artist: "Beauz & Jvna",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "music-4",
+        artist: "Mike Archangelo",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "music-5",
+        artist: "Jim Yosef x Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    },
+
+];
